98179b89566f451383639d93ab078c832be670de
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2021-01-05 21:19:25 +0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.0069.420",
+	name: "The Beginning.",
 }
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
